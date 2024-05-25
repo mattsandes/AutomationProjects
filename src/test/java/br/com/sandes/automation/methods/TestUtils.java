628092a9065e2d414d6d7248858f7c42160f8213
Mateus Sandes <mateussandes@hotmail.com.br>
2024-05-25 19:37:17 -0300

    Criados os metodos assertProductPresentOnCart, assertProductIsNotOnCart e pause

--- src/test/java/br/com/sandes/automation/methods/TestUtils.java
+++ src/test/java/br/com/sandes/automation/methods/TestUtils.java
@@ -2,15 +2,22 @@ package br.com.sandes.automation.methods;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestUtils {
 
     private final WebDriver driver;
+    private final WebDriverWait wait;
 
-    public TestUtils(WebDriver driver) {
+    public TestUtils(WebDriver driver, WebDriverWait wait) {
         this.driver = driver;
+        this.wait = wait;
     }
 
     public void logOnSwagLabs(String username, String password){
@@ -40,5 +47,20 @@ public class TestUtils {
 
     public void removeProductFromCart(String product){
         driver.findElement(By.id("remove-" + product)).click();
+    }
+
+    public void assertProductPresentOnCart(String product){
+        var foundElement = driver.findElement(By.linkText(product)).isDisplayed();
+
+        assertTrue(foundElement);
+    }
+
+    public void assertProductIsNotOnCart(By product){
+
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(product));
+    }
+
+    public void pause(int seconds){
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(seconds));
     }
 }
